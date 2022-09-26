ae9079c40509b55d3333603d8003d849df96b109
bay-bengal <jahidur.rahman@zmartagroup.com>
2022-09-26 15:11:18 +0200

    added localhost-ke, localhost.pem for https

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const https = require('https');
+const fs = require('fs');
+
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Server ready'))
+const options = {
+    key: fs.readFileSync("localhost-key.pem"),
+    cert: fs.readFileSync("localhost.pem"),
+  };
+
+  app.use((req, res, next) => {
+    res.send('<h1>HTTPS is working!</h1>');
+  });
+  
+  const port = 5000;
+  
+  https.createServer(options, app).listen(port, () => {
+    console.log('Server listening on port ' + port);
+  });
+
+
+/* app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3000, () => console.log('Server ready')) */
